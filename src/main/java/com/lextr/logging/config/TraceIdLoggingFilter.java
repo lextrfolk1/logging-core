1523eff738ab3e35960229df79058fc605f56228
Lextr Folk1 <lextrfolk1@gmail.com>
2025-05-31 00:12:59 +0530

    initial commit logging

---
+++ src/main/java/com/lextr/logging/config/TraceIdLoggingFilter.java
@@ -0,0 +1,37 @@
+package com.lextr.logging.config;
+
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanContext;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class TraceIdLoggingFilter implements Filter {
+
+    private static final String TRACE_ID_KEY = "traceId";
+    private static final String SPAN_ID_KEY = "spanId";
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        Span currentSpan = Span.current();
+        SpanContext context = currentSpan.getSpanContext();
+
+        if (context.isValid()) {
+            MDC.put(TRACE_ID_KEY, context.getTraceId());
+            MDC.put(SPAN_ID_KEY, context.getSpanId());
+        }
+
+        try {
+            chain.doFilter(request, response);
+        } finally {
+            MDC.remove(TRACE_ID_KEY);
+            MDC.remove(SPAN_ID_KEY);
+        }
+    }
+}
